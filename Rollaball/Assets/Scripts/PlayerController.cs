2197513f15f82a30ca508dce4017b6d42d0dfed6
Pinatbutter <dpina@harding.edu>
2020-09-07 05:25:52 -0500

    Finished Project
    Had previosuly uploaded the wrong project.

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+using System.Diagnostics;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public int TotalPickups = 13;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+    private bool gameOver = false;
+    private GameObject[] pickups;
+
+    public void youLost(bool timeUp)
+    {
+        gameOver = timeUp;
+        loseTextObject.SetActive(true);
+        GameObject[] pickups = GameObject.FindGameObjectsWithTag("Pickup");
+        foreach (GameObject pick in pickups)
+        {
+            pick.SetActive(false);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+
+        SetCountText();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        gameOver = false;
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        if (gameOver == false)
+        {
+            Vector2 movementVector = movementValue.Get<Vector2>();
+            movementX = movementVector.x;
+            movementY = movementVector.y;
+        }
+    }
+
+    void SetCountText()
+    {
+        if (gameOver == false)
+        {
+            countText.text = "Count: " + count.ToString();
+            if (count >= TotalPickups)
+            {
+                gameOver = true;
+                winTextObject.SetActive(true);
+                GetComponent<TimerCode>().youWon(gameOver);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (gameOver == false)
+        {
+            Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+            rb.AddForce(movement * speed);
+           
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        UnityEngine.Debug.Log(other.gameObject.tag);
+
+        if (other.gameObject.CompareTag("Pickup")) ;
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+}
---
+++ Rollaball/Assets/Scripts/TimerCode.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using TMPro;
+using System.Diagnostics;
+
+
+public class TimerCode : MonoBehaviour
+{
+
+    private float startTime;
+
+    public bool timeFinished;
+    public int TotalSeconds = 0;
+    public TextMeshProUGUI timer;
+    public GameObject playerObject;
+
+    public void youWon(bool won)
+    {
+        timeFinished = won;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        timeFinished = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeFinished == false)
+        {
+            float t = Time.time - startTime;
+            string seconds = ((int)(TotalSeconds - (t % 60))).ToString();
+
+            timer.text = seconds;
+            if (seconds == "0")
+            {
+                timeFinished = true;
+                playerObject.SendMessage("youLost", timeFinished);
+            }
+        }
+    }
+
+}
